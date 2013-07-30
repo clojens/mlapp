ece049d1f449e17274275f0bdfef232d9bbd2a90
supersymmetry <maildumpert@gmail.com>
2013-07-30 19:57:13 +0200

    updated dependency versions

--- project.clj
+++ project.clj
@@ -1,14 +1,13 @@
 (defproject mlapp "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Machine Learning Application"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.0-RC1"]
                  [org.clojure/data.csv "0.1.2"]
-                 [clatrix "0.3.0-SNAPSHOT"]
-                 [net.mikera/core.matrix "0.4.1"]
-                 [incanter "1.4.0"]
+                 [clatrix "0.3.0"]
+                 [net.mikera/core.matrix "0.9.0"]
+                 [incanter "1.5.1"]
                  [inflow "0.1.0-SNAPSHOT"]
-                 [fs "1.3.2"]
-                 [prismatic/plumbing "0.0.1"]
+                 [fs "1.3.3"]
+                 [prismatic/plumbing "0.1.0"]
                  [stemmers "0.2.1"]])
